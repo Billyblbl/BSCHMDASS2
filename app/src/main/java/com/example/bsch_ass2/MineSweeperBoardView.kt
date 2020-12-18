f2233f8e02bab653133a8571dcec9dfce515bc50
Billyblbl <billyblbl.dev@gmail.com>
2020-12-18 22:40:39 +0000

    Added attributes
    - Added board width
    - Added board height
    - Added mine count

--- app/src/main/java/com/example/bsch_ass2/MineSweeperBoardView.kt
+++ app/src/main/java/com/example/bsch_ass2/MineSweeperBoardView.kt
@@ -19,9 +19,19 @@ class MineSweeperBoardView @JvmOverloads constructor(
     var totalMines = 0
     var markedCells = 0
     var mode = Mode.UNCOVER
+    var boardWidth = 0
+    var boardHeight = 0
 
     init {
-        //Act as constructor here
+        context.theme.obtainStyledAttributes(attrs, R.styleable.MineSweeperBoardView, style, 0).apply {
+            try {
+                boardWidth = getInteger(R.styleable.MineSweeperBoardView_board_width, 10)
+                boardHeight = getInteger(R.styleable.MineSweeperBoardView_board_height, 10)
+                totalMines = getInteger(R.styleable.MineSweeperBoardView_mine_count, 20)
+            } finally {
+                recycle()
+            }
+        }
     }
 
     fun toggleMode() { mode = if (mode == Mode.UNCOVER) Mode.MARK else Mode.UNCOVER }
