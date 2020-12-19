57f5e18e392d3d13483cb20418415c17fe1d2c92
Billyblbl <billyblbl.dev@gmail.com>
2020-12-19 04:07:43 +0000

    Milestone 3: Basic touch behavior
    - Added cells array
    - Added CellState enum
    - Added cell rendering with color based on cell state
    - Added on touch cell state change
    - Fixed missing view invalidate in rendering data update
    - Fixed division by zero in rendering data update

--- app/src/main/java/com/example/bsch_ass2/MineSweeperBoardView.kt
+++ app/src/main/java/com/example/bsch_ass2/MineSweeperBoardView.kt
@@ -6,6 +6,7 @@ import android.graphics.Color
 import android.graphics.Paint
 import android.graphics.Rect
 import android.util.AttributeSet
+import android.view.MotionEvent
 import android.view.View
 
 class MineSweeperBoardView @JvmOverloads constructor(
@@ -26,12 +27,14 @@ class MineSweeperBoardView @JvmOverloads constructor(
     var boardWidth = 0
         set(value) {
             field = value
+            resetBoard()
             updateRenderingData()
         }
 
     var boardHeight = 0
         set(value) {
             field = value
+            resetBoard()
             updateRenderingData()
         }
 
@@ -40,6 +43,13 @@ class MineSweeperBoardView @JvmOverloads constructor(
             field = value
             updateRenderingData()
         }
+
+    enum class CellState(val drawColor : Int) {
+        UNKNOWN(Color.BLACK),
+        UNCOVERED(Color.GRAY)
+    }
+
+    var cells = Array(boardWidth * boardHeight) { CellState.UNKNOWN }
 
     override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
         super.onSizeChanged(w, h, oldw, oldh)
@@ -57,23 +67,32 @@ class MineSweeperBoardView @JvmOverloads constructor(
 
     fun toggleMode() { mode = if (mode == Mode.UNCOVER) Mode.MARK else Mode.UNCOVER }
 
+    fun resetBoard() {
+        cells = Array(boardWidth * boardHeight) { CellState.UNKNOWN }
+    }
+
+    private fun cellIndexFromCoords(x : Int, y : Int) = y * boardWidth + x
+
+    fun at(x : Int, y : Int) = cells[cellIndexFromCoords(x, y)]
+
+    private var contentWidth = width - paddingLeft - paddingRight
+    private var contentHeight = height - paddingTop - paddingBottom
     private var fullCellRect = Rect()
     private var visCellRect = Rect()
     fun updateRenderingData() {
-        val contentWidth = width - paddingLeft - paddingRight
-        val contentHeight = height - paddingTop - paddingBottom
+        contentWidth = width - paddingLeft - paddingRight
+        contentHeight = height - paddingTop - paddingBottom
 
-        fullCellRect = Rect(
-            0, 0,
-            contentWidth / boardWidth,
-            contentHeight / boardHeight
-        )
+        fullCellRect =  if (boardWidth == 0 || boardHeight == 0 ) Rect(0, 0, 0, 0)
+                        else Rect(0, 0,contentWidth / boardWidth,contentHeight / boardHeight)
+
         visCellRect = Rect(
             cellMargins.toInt(),
             cellMargins.toInt(),
             (fullCellRect.width() - cellMargins).toInt(),
             (fullCellRect.height() - cellMargins).toInt()
         )
+        invalidate()
     }
 
     private val cellPaint = Paint()
@@ -87,7 +106,6 @@ class MineSweeperBoardView @JvmOverloads constructor(
 
         canvas.save()
         canvas.translate(cellMargins, cellMargins)
-        cellPaint.color = Color.BLACK
         for (i in 0 until boardHeight) {
             canvas.save()
             for (j in 0 until boardWidth) {
@@ -95,6 +113,7 @@ class MineSweeperBoardView @JvmOverloads constructor(
                 canvas.translate((fullCellRect.width() * j).toFloat(), (fullCellRect.height() * i).toFloat())
 
                 //Draw cell
+                cellPaint.color = at(j, i).drawColor
                 canvas.drawRect(visCellRect, cellPaint)
 
                 canvas.restore()
@@ -102,6 +121,37 @@ class MineSweeperBoardView @JvmOverloads constructor(
             canvas.restore()
         }
         canvas.restore()
+    }
+
+    override fun onTouchEvent(event: MotionEvent?): Boolean {
+        if (event == null) return super.onTouchEvent(event)
+        val x = (event.x * boardWidth / contentWidth).toInt()
+        val y = (event.y * boardHeight / contentHeight).toInt()
+        onTouchCellEvent(x, y)
+        return super.onTouchEvent(event)
+    }
+
+    private fun onTouchCellEvent(x : Int, y : Int) {
+        val state = cells[cellIndexFromCoords(x, y)]
+        when {
+            state == CellState.UNKNOWN && mode == Mode.UNCOVER -> uncover(x, y)
+            else -> {}
+        }
+    }
+
+    private fun uncover(x : Int, y : Int) {
+        //if mine in (x, y) -> boom, else
+        cells[cellIndexFromCoords(x, y)] = CellState.UNCOVERED
+        //propagate uncover depending on adjacency
+        invalidate()
+    }
+
+    private fun mark(x : Int, y : Int) {
+
+    }
+
+    private fun unmark(x : Int, y : Int) {
+
     }
 
 }
